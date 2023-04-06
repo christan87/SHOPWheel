58d75ae8a8ab6b4c9a9e835e2b4f7e0282b90108
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2023-04-06 02:08:13 -0700

    created the contexts/Socket dir and all the socket.io context files. created the client/app/constants dir and the index.js file in it.

---
+++ client/app/constants/index.js
@@ -0,0 +1,6 @@
+export const BASE_API_URL = process.env.BASE_API_URL;
+
+export const SOCKET_URL = 
+    window.location.host.indexOf('localhost') >= 0
+        ? 'http://127.0.0.1:3000'
+        : window.location.host;
