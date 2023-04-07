796aa4404e23b0369dacb6ccebe50e5e2f69cf89
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2023-04-07 01:17:20 -0700

    Created the actions.js in containers/Authentication. Created the actions.js in containers/Authentication. Created the index.js in containers/Authentication. Updated reducers with applicationReducer and authentiactionReducer. Corrected spelling error in dev script in package.json

--- client/app/reducers.js
+++ client/app/reducers.js
@@ -10,10 +10,15 @@ import { reducer as notifications } from 'react-notification-system-redux';
 
 // import reducers
 
+import applicationReducer from './containers/Application/reducer';
+import authenticationReducer from './containers/Authentication/reducer';
+
 const createReducer = history => 
     combineReducers({
         router: connectRouter(history),
-        notifications
+        notifications,
+        application: applicationReducer,
+        authentication: authenticationReducer
     });
 
 export default createReducer;
