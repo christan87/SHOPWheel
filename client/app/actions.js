bd2918848740b7a6ac3cece87b6a20469cdc7412
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2023-04-14 03:30:08 -0700

    added navigation actions to actions.js in client/app

--- client/app/actions.js
+++ client/app/actions.js
@@ -8,13 +8,15 @@ import { bindActionCreators } from 'redux';
 import * as application from './containers/Application/actions';
 import * as authentication from './containers/Authentication/actions';
 import * as homepage from './containers/Homepage/actions';
+import * as navigation from './containers/Navigation/actions';
 
 export default function (dispatch) {
     return bindActionCreators(
         {
             ...application,
             ...authentication,
-            ...homepage
+            ...homepage,
+            ...navigation
         },
         dispatch
     );
