2b63deb414e77bfe6b78579404fd68fff51909bf
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2023-04-15 05:55:18 -0700

    created the components/Common/Popover dir and index.js so that I can creaate the buttom component

---
+++ client/app/components/Common/Popover/index.js
@@ -0,0 +1,24 @@
+/*
+* Popover
+*/ 
+
+import React from "react";
+
+import { UncontrolledPopover, PopoverHeader, PopoverBody } from "reactstrap";
+
+const Popover = props => {
+    const { target, placement, popoverTitle, children } = props;
+
+    return(
+        <UncontrolledPopover placement={placement} target={target} trigger='legacy'>
+            {popoverTitle && <PopoverHeader>{popoverTitle}</PopoverHeader>}
+            <PopoverBody>{children}</PopoverBody>
+        </UncontrolledPopover>
+    );
+};
+
+Tooltip.defaultProps ={
+    placement: 'top'
+};
+
+export default Popover;
