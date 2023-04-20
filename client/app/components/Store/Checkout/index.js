a6278e41c468f3fd92f70544483698bcfa618ace
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2023-04-20 07:05:22 -0700

    created client/app/components/Store/CartSummary/index.js and client/app/components/Store/Checkout/index.js

---
+++ client/app/components/Store/Checkout/index.js
@@ -0,0 +1,38 @@
+/*
+* Checkout
+*/ 
+
+import React from 'react';
+
+import Button from '../../Common/Button';
+
+const Checkout = () => {
+    const { authenticated, handleShopping, handleCheckout, placeOrder } = props;
+
+    return (
+        <div className='easy-checkout'>
+            <div className='checkout-actions'>
+                <Button
+                    variant='primary'
+                    text='Continue Shopping'
+                    onClick={() => handleShopping()}
+                />
+                {authenticated ? (
+                    <Button
+                        variant='primary'
+                        text='Place Order'
+                        onClick={() => placeOrder()}
+                    />
+                ) : (
+                    <Button
+                        variant='primary'
+                        text='Proceed To Checkout'
+                        onClick={() => handleCheckout()}
+                    />
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Checkout;
