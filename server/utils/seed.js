7900d6e430ec0e14f47ecf0c2ad15de397445f62
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2023-05-13 07:56:34 -0700

    created the models/user file and schema for mongoDB, created the utils/seed file to seed the database with a single admin user and added the script to run the seed file to the package.json

---
+++ server/utils/seed.js
@@ -0,0 +1,42 @@
+const chalk = require('chalk');
+const bcrypt = require('bcryptjs');
+
+const setupDB = require('./db');
+const  { ROLE_ADMIN } = require('../constants');
+const User = require('../models/user');
+
+const seedDB = async () => {
+    try {
+        const user = new User({
+            email: 'admin@shopwheel.com',
+            password: 'test123',
+            firstName: 'admin',
+            lastName: 'admin',
+            role: ROLE_ADMIN
+        });
+
+        const existingUser = await User.findOne({ email: user.email });
+        console.log(`existingUser: ${existingUser}`);
+        if(existingUser) throw new Error('user collection has already been seeded.');
+
+        /* Hash / Secure User Password */ 
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(user.password, salt);
+        user.password = hash;
+
+        await user.save();
+
+        console.log(`${chalk.green('✓')} ${chalk.green('DB has finished seeding!')}`);
+
+    } catch (error) {
+        console.log(`${chalk.red('x')} ${chalk.green('Experienced error while seeding database.')}`);
+        console.log(error);
+        return null;
+    }
+};
+
+(async () => {
+    await setupDB().then(async () => {
+        await seedDB();
+    });
+})();
