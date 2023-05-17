5643e8577ba9293930b09c103bb92cab940207ff
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2023-05-17 07:42:43 -0700

    created the SignupProvider component for fb/google signin, the login container and scss

--- client/app/containers/Login/index.js
+++ client/app/containers/Login/index.js
@@ -1,31 +1,35 @@
 import React from 'react';
 
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { Row, Col } from 'reactstrap';
 
 // dont forget to add the actions from this dir to the referenced actions file
 import actions from '../../actions';
 
 import Input from '../../components/Common/Input';
+import Button from '../../components/Common/Button';
+import SignupProvider from '../../components/Common/SignupProvider';
 
 class Login extends React.PureComponent {
     
     render() {
         const {} = this.props;
-        const removeStyle = {
-            outline: 'red dashed 1px'
-        };
         return (
             <div className='login-form'>
                 <h2>Login Form</h2>
                 <hr />
                 <form onSubmit={()=> alert('Form Submitted')}>
-                    <Row style={removeStyle} >
-                        <Col style={removeStyle} >
-                            <Col style={removeStyle} >
+                    <Row>
+                        <Col
+                            xs={{size: 12, order: 2}}
+                            md={{size: 6, order: 1}}
+                            className='p-0'
+                        >
+                            <Col xs='12' md='12' >
                                 <Input 
                                     type={'text'}
-                                    error={{}}
+                                    //error={{}}
                                     label={'Email Adress'}
                                     name={'email'}
                                     placeholder={'Email...'}
@@ -35,8 +39,50 @@ class Login extends React.PureComponent {
                                     }}
                                 />
                             </Col>
+                            <Col xs='12' md='12' >
+                                <Input 
+                                    type={'password'}
+                                    //error={{}}
+                                    label={'Password'}
+                                    name={'password'}
+                                    placeholder={'Password...'}
+                                    value={{}}
+                                    onInputChange={(name, value) => {
+                                        //some function??
+                                    }}
+                                />
+                            </Col>
+                        </Col>
+                        <Col
+                            xs={{size: 12, order: 1}}
+                            md={{size: 6, order: 2}}
+                            className='mb-2 mb-md-0'
+                        >
+                            <SignupProvider />
                         </Col>
                     </Row>
+                    <hr />
+                    <div className='d-flex flex-column flex-md-row align-items-md-center justify-conent-between'>
+                        <div>
+                            <Button 
+                                type='submit'
+                                variant='primary'
+                                text='Login'
+                                disabled={false}
+                            />
+                        </div>
+                        <Button 
+                            variant='link'
+                            text='Create an Account'
+                            onClick={()=>alert('Create an Account Clicked!')}
+                        />
+                        <Link
+                            className='redirect-link forgot-password-link'
+                            to={'#/forgot-password'}
+                        >
+                            Forgot Password?
+                        </Link>
+                    </div>
                 </form>
             </div>
         )
