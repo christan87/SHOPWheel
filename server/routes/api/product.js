1bfb0b76113572da4e5b180c14495be0192848a0
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2023-07-06 07:00:06 -0700

    Created server/utils/queries.js, Created server/utils/storage.js, Created server/middleware/role.js, Created server/models/category.js, Created server/models/brand.js, Created server/models/product.js, Created server/utils/auth.js and Created server/routes/api/product.js

---
+++ server/routes/api/product.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const Mongoose = require('mongoose');
+
+//Bring in Models & Utils
+
+const Product = require('../../models/product');
+const Brand = require('../../models/brand');
+const Category = require('../../models/category');
+const auth = require('../../middleware/auth');
+const role = require('../../middleware/role');
+const checkAuth = require('../../utils/auth');
+const { s3Upload } = require('../../utils/storage');
+const {
+    getStoreProductsQuery,
+    getStoreProductsWishListQuery
+} = require('../../utils/queries');
+const { ROLES } = require('../../constants');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
